86797f7823d42622139847e7b79d3f859d5a78a8
timurlog <timur.logie@gmail.com>
2024-01-28 13:32:46 +0100

    added class.js => class Pokemon created, added attacks button

---
+++ public/js/class.js
@@ -0,0 +1,11 @@
+export class Pokemon {
+    constructor(name, hp, hpMax, attackOne, attackTwo, attackThree, attackFour) {
+        this.name = name;
+        this.hp = hp;
+        this.hpMax = hpMax;
+        this.attackOne = attackOne;
+        this.attackTwo = attackTwo;
+        this.attackThree = attackThree;
+        this.attackFour = attackFour;
+    }
+}
--- public/js/instance.js
+++ public/js/instance.js
@@ -1,3 +1,5 @@
+import { Pokemon } from "./class.js";
+
 // STAR MENU
 export let startMenu = document.querySelector('.start-menu');
 export let startMenuTitle = document.querySelector('#startMenuTitle');
@@ -27,6 +29,40 @@ export let playerRT = document.querySelector('#playerRT');
 export let fightGameplay = document.querySelector('.fight-gameplay');
 export let fightGameplayScreenTop = document.querySelector('.fight-gameplay .screen-top');
 
+// BLAZIKEN ATTACKS
+export let blazikenAttacks = document.querySelector('#blazikenAttacks');
+export let blazikenAttackOneBtn = document.querySelector('#blazikenAttackOne');
+export let blazikenAttackTwoBtn = document.querySelector('#blazikenAttackTwo');
+export let blazikenAttackThreeBtn = document.querySelector('#blazikenAttackThree');
+export let blazikenAttackFourBtn = document.querySelector('#blazikenAttackFour');
+
+// RAYQUAZA ATTACKS
+export let rayquazaAttacks = document.querySelector('#rayquazaAttacks');
+export let rayquazaAttackOneBtn = document.querySelector('#rayquazaAttackOne');
+export let rayquazaAttackTwoBtn = document.querySelector('#rayquazaAttackTwo');
+export let rayquazaAttackThreeBtn = document.querySelector('#rayquazaAttackThree');
+export let rayquazaAttackFourBtn = document.querySelector('#rayquazaAttackFour');
+
+// GARDEVOIR ATTACKS
+export let gardevoirAttacks = document.querySelector('#gardevoirAttacks');
+export let gardevoirAttackOneBtn = document.querySelector('#gardevoirAttackOne');
+export let gardevoirAttackTwoBtn = document.querySelector('#gardevoirAttackTwo');
+export let gardevoirAttackThreeBtn = document.querySelector('#gardevoirAttackThree');
+export let gardevoirAttackFourBtn = document.querySelector('#gardevoirAttackFour');
+
+// DEOXYS ATTACKS
+export let deoxysAttacks = document.querySelector('#deoxysAttacks');
+export let deoxysAttackOneBtn = document.querySelector('#deoxysAttackOne');
+export let deoxysAttackTwoBtn = document.querySelector('#deoxysAttackTwo');
+export let deoxysAttackThreeBtn = document.querySelector('#deoxysAttackThree');
+export let deoxysAttackFourBtn = document.querySelector('#deoxysAttackFour');
+
+//POKEMON
+export let blaziken = new Pokemon("Blaziken", 187, 187, 85, 120, false, 85);
+export let rayquaza = new Pokemon("Rayquaza", 414, 414, 120, 90, 80, 120);
+export let gardevoir = new Pokemon("Gardevoir", 175, 175, 90, 120, false, 95);
+export let deoxys = new Pokemon("Deoxys", 304, 304, 140, 120, 120, 150);
+
 // SFX
 export let buttonSfx = new Audio('./public/assets/music/sfx/button.wav');
 export let selectThemeSfx = new Audio('./public/assets/music/sfx/select-menu-theme.wav');
@@ -53,4 +89,4 @@ export let arenaArray = [
     "url('./public/assets/image/arena/arena-16.png')",
     "url('./public/assets/image/arena/arena-17.png')",
     "url('./public/assets/image/arena/arena-18.png')"
-]
+];
